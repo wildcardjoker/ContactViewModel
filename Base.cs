71705e1ac837bbadb0535198658e363442647f4c
wildcardjoker <wildcardjoker@users.noreply.github.com>
2015-07-25 21:08:36 +0800

    Add basic ViewModel layout, including New Contact command, Person collection, and individual person.

---
+++ Base.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) 2015 WildCardJoker
+// Licensed under the MIT License: http://opensource.org/licenses/MIT
+// Created: 2015-07-12
+// Last Modified: 2015-07-25-8:22 PM
+
+#region Using Directives
+
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ContactViewModel.Annotations;
+using wcj;
+
+#endregion
+
+namespace ContactViewModel
+{
+    public class Base : INotifyPropertyChanged, INotifyCollectionChanged
+    {
+        #region Fields
+
+        private Person _contactPerson;
+        private ICommand _newContactCommand;
+        private ObservableCollection<Person> _persons;
+
+        #endregion
+
+        #region Properties
+
+        public Person ContactPerson
+        {
+            get { return _contactPerson; }
+            set
+            {
+                _contactPerson = value;
+                OnPropertyChanged("ContactPerson");
+            }
+        }
+
+        public ICommand NewContactCommand => _newContactCommand ??
+                                             (_newContactCommand =
+                                                 new RelayCommand(param => AddNewContact()));
+
+        public ObservableCollection<Person> Persons
+        {
+            get { return _persons; }
+            set
+            {
+                _persons = value;
+                OnPropertyChanged("Persons");
+            }
+        }
+
+        #endregion
+
+        #region INotifyCollectionChanged Members
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        private void AddNewContact()
+        {
+            ContactPerson = new Person();
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RelayCommand.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2015 WildCardJoker
+// Licensed under the MIT License: http://opensource.org/licenses/MIT
+// Created: 2015-07-14
+// Last Modified: 2015-07-25-8:28 PM
+
+#region Using Directives
+
+using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+#endregion
+
+namespace ContactViewModel
+{
+    /// <summary>
+    ///     A command whose sole purpose is to relay its functionality to other
+    ///     objects by invoking delegates. The default return value for the
+    ///     CanExecute method is 'true'.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameters)
+        {
+            return _canExecute == null || _canExecute(parameters);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameters)
+        {
+            _execute(parameters);
+        }
+
+        #endregion
+    }
+}
